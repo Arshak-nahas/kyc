2de069c303f4b099c17ed67d161a12d543b59ea7
don-k-jacob <donkjacob.business@gmail.com>
2020-06-03 22:30:28 +0530

    Bug Fixed
    the bug was the first home screen was pointing to same total screen ie Welcome Screen

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:kyc/home.dart';
 import 'photo_screen.dart';
 import 'video_screen.dart';
 
@@ -12,7 +13,7 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   int _currentIndex = 0;
   final List<Widget> _children = [
-    WelcomeScreen(),
+    Home(),
     PhotoScreen(),
     VideoScreen(),
     PhotoScreen()
@@ -41,68 +42,68 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         ],
       ),
       backgroundColor: Colors.red,
-      body: Padding(
-        padding: EdgeInsets.all(25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, WelcomeScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'welcome In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, PhotoScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'photo In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, VideoScreen.id);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'video In',
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+//      body: Padding(
+//        padding: EdgeInsets.all(25),
+//        child: Column(
+//          crossAxisAlignment: CrossAxisAlignment.center,
+//          children: <Widget>[
+//            Padding(
+//              padding: EdgeInsets.symmetric(vertical: 16.0),
+//              child: Material(
+//                elevation: 5.0,
+//                color: Colors.lightBlueAccent,
+//                borderRadius: BorderRadius.circular(30.0),
+//                child: MaterialButton(
+//                  onPressed: () {
+//                    Navigator.pushNamed(context, WelcomeScreen.id);
+//                  },
+//                  minWidth: 200.0,
+//                  height: 42.0,
+//                  child: Text(
+//                    'welcome In',
+//                  ),
+//                ),
+//              ),
+//            ),
+//            Padding(
+//              padding: EdgeInsets.symmetric(vertical: 16.0),
+//              child: Material(
+//                elevation: 5.0,
+//                color: Colors.lightBlueAccent,
+//                borderRadius: BorderRadius.circular(30.0),
+//                child: MaterialButton(
+//                  onPressed: () {
+//                    Navigator.pushNamed(context, PhotoScreen.id);
+//                  },
+//                  minWidth: 200.0,
+//                  height: 42.0,
+//                  child: Text(
+//                    'photo In',
+//                  ),
+//                ),
+//              ),
+//            ),
+//            Padding(
+//              padding: EdgeInsets.symmetric(vertical: 16.0),
+//              child: Material(
+//                elevation: 5.0,
+//                color: Colors.lightBlueAccent,
+//                borderRadius: BorderRadius.circular(30.0),
+//                child: MaterialButton(
+//                  onPressed: () {
+//                    Navigator.pushNamed(context, VideoScreen.id);
+//                  },
+//                  minWidth: 200.0,
+//                  height: 42.0,
+//                  child: Text(
+//                    'video In',
+//                  ),
+//                ),
+//              ),
+//            ),
+//          ],
+//        ),
+//      ),
     );
   }
 }
